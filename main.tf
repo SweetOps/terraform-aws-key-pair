7f343b3475b22dcf61b9e870917cf4660881bca7
SweetOps <vladimir.fess@gmail.com>
2018-11-01 12:58:11 +0200

    Add upload keys to s3

--- main.tf
+++ main.tf
@@ -1,22 +1,18 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  attributes = "${var.attributes}"
-  delimiter  = "${var.delimiter}"
-  tags       = "${var.tags}"
+  source      = "git::https://github.com/SweetOps/terraform-null-label.git?ref=tags/0.5.4"
+  namespace   = "${var.namespace}"
+  stage       = "${var.stage}"
+  environment = "${var.environment}"
+  name        = "${var.name}"
+  attributes  = "${var.attributes}"
+  delimiter   = "${var.delimiter}"
+  tags        = "${var.tags}"
+  enabled     = "${var.generate_ssh_key}"
 }
 
 locals {
   public_key_filename  = "${var.ssh_public_key_path}/${module.label.id}${var.public_key_extension}"
   private_key_filename = "${var.ssh_public_key_path}/${module.label.id}${var.private_key_extension}"
-}
-
-resource "aws_key_pair" "imported" {
-  count      = "${var.generate_ssh_key == "false" ? 1 : 0}"
-  key_name   = "${module.label.id}"
-  public_key = "${file("${local.public_key_filename}")}"
 }
 
 resource "tls_private_key" "default" {
@@ -52,4 +48,24 @@ resource "null_resource" "chmod" {
   provisioner "local-exec" {
     command = "${format(var.chmod_command, local.private_key_filename)}"
   }
+}
+
+resource "aws_s3_bucket_object" "public_key" {
+  count      = "${var.generate_ssh_key == "true" && var.bucket != "" ? 1 : 0}"
+  depends_on = ["local_file.public_key_openssh"]
+  key        = "${local.public_key_filename}"
+  bucket     = "${var.bucket}"
+  source     = "${local.public_key_filename}"
+  etag       = "${md5(file(local.public_key_filename))}"
+  tags       = "${module.label.tags}"
+}
+
+resource "aws_s3_bucket_object" "private_key" {
+  count      = "${var.generate_ssh_key == "true" && var.bucket != "" ? 1 : 0}"
+  depends_on = ["local_file.private_key_pem"]
+  key        = "${local.private_key_filename}"
+  bucket     = "${var.bucket}"
+  source     = "${local.private_key_filename}"
+  etag       = "${md5(file(local.private_key_filename))}"
+  tags       = "${module.label.tags}"
 }
